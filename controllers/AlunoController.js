15b977f834628109ca1e6e71bc872aeee7473205
Bernardo-Vidal <bernardorosa.bg015@academico.ifsul.edu.br>
2025-08-14 10:53:56 -0300

    AlunoController

--- controllers/AlunoController.js
+++ controllers/AlunoController.js
@@ -14,5 +14,9 @@ export default class AlunoController{
             })
             res.redirect('/' + caminhoBase + 'add')
         }
+        this.list = async(req,res)=>{
+            const resultado = await Aluno.find({})
+            res.render(caminhoBase + 'lst', {Alunos:resultado})
+        }
     }
 }
